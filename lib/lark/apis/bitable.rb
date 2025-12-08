3f713c36bdcbcb828882644a9b5e1ac87264dfd0
yingchao.wu <yingchao.wu@dreamsmart.com>
2025-12-08 10:56:15 +0800

    Add user_id_type parameter to Bitable create and update methods

--- lib/lark/apis/bitable.rb
+++ lib/lark/apis/bitable.rb
@@ -1,15 +1,15 @@
 module Lark
   module Apis
     module Bitable
-      def update(app_token, table_id, record_id, fields)
+      def update(app_token, table_id, record_id, user_id_type, fields)
         put "bitable/v1/apps/#{app_token}/tables/#{table_id}/records/#{record_id}", {
           fields: fields
         }
       end
 
-      def create(app_token, table_id, fields)
+      def create(app_token, table_id, user_id_type, fields)
         puts("Creating record in Bitable app_token: #{app_token}, table_id: #{table_id}, fields: #{fields}")
-        post "bitable/v1/apps/#{app_token}/tables/#{table_id}/records/", {
+        post "bitable/v1/apps/#{app_token}/tables/#{table_id}/records?user_id_type=#{user_id_type}", {
           fields: fields
         }
       end
