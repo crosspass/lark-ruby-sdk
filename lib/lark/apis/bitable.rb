986144f80120ff895a84173ea012bc5470f7b10f
yingchao.wu <yingchao.wu@dreamsmart.com>
2026-01-16 14:11:09 +0800

    Add batch_get_records method to Bitable API

--- lib/lark/apis/bitable.rb
+++ lib/lark/apis/bitable.rb
@@ -1,6 +1,13 @@
 module Lark
   module Apis
     module Bitable
+      def batch_get_records(app_token, table_id, record_ids, user_id_type)
+        post "bitable/v1/apps/#{app_token}/tables/#{table_id}/records/batch_get?", {
+          record_ids: record_ids,
+          user_id_type: user_id_type
+        }
+      end
+
       def update(app_token, table_id, record_id, user_id_type, fields)
         put "bitable/v1/apps/#{app_token}/tables/#{table_id}/records/#{record_id}?user_id_type=#{user_id_type}", {
           fields: fields
