abcf42cb5cc8d541a288fa892b8d697c367c6438
rails <rails@redmine-rails-176-64>
2026-06-25 19:55:17 +0800

    Add batch operations and field management to Bitable API

--- lib/lark/apis/bitable.rb
+++ lib/lark/apis/bitable.rb
@@ -15,14 +15,55 @@ module Lark
       end
 
       def create(app_token, table_id, user_id_type, fields)
-        puts("Creating record in Bitable app_token: #{app_token}, table_id: #{table_id}, fields: #{fields}")
         post "bitable/v1/apps/#{app_token}/tables/#{table_id}/records?user_id_type=#{user_id_type}", {
           fields: fields
         }
       end
 
+      def delete_record(app_token, table_id, record_id)
+        delete "bitable/v1/apps/#{app_token}/tables/#{table_id}/records/#{record_id}"
+      end
+
+      def batch_create_records(app_token, table_id, user_id_type, records)
+        post "bitable/v1/apps/#{app_token}/tables/#{table_id}/records/batch_create?user_id_type=#{user_id_type}", {
+          records: records
+        }
+      end
+
+      def batch_update_records(app_token, table_id, user_id_type, records)
+        post "bitable/v1/apps/#{app_token}/tables/#{table_id}/records/batch_update?user_id_type=#{user_id_type}", {
+          records: records
+        }
+      end
+
+      def batch_delete_records(app_token, table_id, records)
+        post "bitable/v1/apps/#{app_token}/tables/#{table_id}/records/batch_delete", {
+          records: records
+        }
+      end
+
+      def search_records(app_token, table_id, query = {})
+        post "bitable/v1/apps/#{app_token}/tables/#{table_id}/records/search", query
+      end
+
+      def fields(app_token, table_id)
+        get "bitable/v1/apps/#{app_token}/tables/#{table_id}/fields"
+      end
+
+      def create_field(app_token, table_id, field)
+        post "bitable/v1/apps/#{app_token}/tables/#{table_id}/fields", field
+      end
+
+      def update_field(app_token, table_id, field_id, field)
+        put "bitable/v1/apps/#{app_token}/tables/#{table_id}/fields/#{field_id}", field
+      end
+
+      def delete_field(app_token, table_id, field_id)
+        delete "bitable/v1/apps/#{app_token}/tables/#{table_id}/fields/#{field_id}"
+      end
+
       # 获取知识空间节点信息
-      def wiki_node_info(tenant_access_token, node_token)
+      def wiki_node_info(node_token)
         get "wiki/v2/spaces/get_node?token=#{node_token}"
       end
     end
